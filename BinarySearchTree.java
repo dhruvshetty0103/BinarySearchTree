6f2dae417864f29cc836e66697d8d563838698f5
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-09-12 18:55:32 +0530

    Added:More nodes are being added and size of binary tree is returned

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -63,6 +63,15 @@ public class BinarySearchTree<K>
 		}
 	}
 
+	public int getSize() {
+		return this.getSizeRecursive(root);
+	}
+
+	private int getSizeRecursive(Node current) {
+		return current == null ? 0
+				: 1 + this.getSizeRecursive(current.leftNode) + this.getSizeRecursive(current.rightNode);
+	}
+
 	public static void main(String[] args)
 	{
 		/* Start with the empty list. */
@@ -72,6 +81,17 @@ public class BinarySearchTree<K>
 		tree.insert(56);
 		tree.insert(70);
 		tree.insert(30);
+		tree.insert(22);
+		tree.insert(40);
+		tree.insert(11);
+		tree.insert(3);
+		tree.insert(16);
+		tree.insert(60);
+		tree.insert(95);
+		tree.insert(65);
+		tree.insert(63);
+		tree.insert(67);
+		System.out.println("Binary search tree size:" + tree.getSize());
 		// Print the LinkedList
 		tree.inorder();
 	}
